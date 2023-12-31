fd179369f6d78b02ce014ba86da64d48e04e72dc
Vagelis Papadimitriou <papadimitriou.vg@gmail.com>
2023-12-31 22:54:13 +0200

    Add project files.

---
+++ Models/Player.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PointCounting.Models
+{
+    public class Player
+    {
+        public int PlayerId { get; set; }
+        public string Name { get; set; }
+        public int Score { get; set; }
+    }
+}
---
+++ MyDatabase/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace PointCounting.MyDatabase
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext() : base("Sindesmos")
+        {
+            
+        }
+
+        //public DbSet<Student> Students { get; set; }
+    }
+}
